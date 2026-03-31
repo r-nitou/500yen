6bb7f1504fdabb274f6839c05a051a81fb59fbef
DWE-r-matsura <r_matsura@dworks-ent.co.jp>
2026-03-31 15:08:29 +0900

    妹の部屋　仮画像差し替え、adv,ボタンの制御など

--- Assets/ADV/Scripts/AdvController.cs
+++ Assets/ADV/Scripts/AdvController.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 using Utage;
 
 public class AdvController : MonoBehaviour
 {
     [Header("UtageのAdvEngine"), SerializeField]
     private AdvEngine advEngine = null;
-    
+
+    private UnityEvent onPlayFinish_ = new UnityEvent();//PlayADVの前に登録しておく
+    public UnityEvent OnPlayFinish_ => onPlayFinish_; 
+
     private bool isPlaying = false;// 再生中フラグ
 
     /// <summary>
@@ -16,9 +20,14 @@ public class AdvController : MonoBehaviour
     public void PlayADV(string scenarioLabel)
     {
         // 既にシナリオ再生中、またはエンジンが初期化されていない場合は処理しない
-        if (advEngine.IsWaitBootLoading || isPlaying)
+        if (isPlaying)
         {
-            Debug.LogWarning("ADVエンジンの準備ができていないか、既に再生中です。");
+            Debug.LogWarning("ADVエンジンは既に再生中です。");
+            return;
+        }
+        if (advEngine.IsWaitBootLoading)
+        {
+            Debug.LogWarning("ADVエンジンの準備ができていないです。");
             return;
         }
 
@@ -30,26 +39,28 @@ public class AdvController : MonoBehaviour
         return isPlaying;
     }
 
+    public bool CanPlay()
+    {
+        return !(advEngine.IsWaitBootLoading || isPlaying);
+    }
+
     private IEnumerator PlayScenario(string scenarioLabel)
     {
-        // 1. ADVのUIを表示する
+        // 1. 指定したラベルからシナリオを再生開始
         isPlaying = true;
-
-        // 2. 指定したラベルからシナリオを再生開始
         advEngine.JumpScenario(scenarioLabel);
-
-        // ※ JumpScenarioを呼んだ直後はフラグが切り替わっていないことがあるため、1フレーム待つ
         yield return null;
 
-        // 3. シナリオが終了するまで待機
+        // 2. シナリオが終了するまで待機
         while (!advEngine.IsEndScenario)
         {
             yield return null;
         }
 
-        // 4. シナリオが終了したら、ADVのUIを非表示にして元に戻す
-        isPlaying = false;
-
+        // 3. シナリオ終了
         Debug.Log("シナリオ再生が終了し、元の画面に戻りました。");
+        isPlaying = false;
+        OnPlayFinish_?.Invoke();
+        OnPlayFinish_.RemoveAllListeners();
     }
 }
---
+++ Assets/ADV/Scripts/HouseYoungerSister.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class HouseYoungerSister: MonoBehaviour
+{
+    [SerializeField, Header("ADV")]
+    private AdvController advController_ = null;
+
+    [SerializeField, Header("表示を切り替えるボタンのリスト")]
+    private Button[] buttonList_ = null;
+
+    private bool needInvoke_ = true;
+
+
+    private void Awake()
+    {
+        foreach (Button button in buttonList_)
+        {
+            button.gameObject.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        // Startでは再生できなかったのでここで呼ぶ
+        if (advController_.CanPlay() && needInvoke_)
+        {
+            needInvoke_ = false;
+            advController_.OnPlayFinish_.AddListener(OnEnterADVFinish);
+            advController_.PlayADV("H_001_01");
+        }
+    }
+
+    public void OnPressButtonEvent(int index)
+    {
+        switch (index)
+        {
+            // 機嫌
+            case 1:
+                break;
+
+            // 健康
+            case 2:
+                break;
+
+            // メンタル
+            case 3:
+                break;
+
+            default:
+                Debug.Log("YoungerSyster::OnPressButtonEvent -> case文がdefaultです");
+                return;
+        }
+
+        string id = index.ToString("00");
+        advController_.PlayADV("H_101_" + id);
+    }
+
+    private void OnEnterADVFinish()
+    {
+        foreach (Button button in buttonList_)
+        {
+            button.gameObject.SetActive(true);
+        }
+    }
+}
